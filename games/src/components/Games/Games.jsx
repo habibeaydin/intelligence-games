7ce66d573eb792c86e7b06b64395aa498a42541f
Habibe Aydın <habibeydn@gmail.com>
2024-12-15 13:54:03 +0300

    navbar is edited

--- games/src/components/Games/Games.jsx
+++ games/src/components/Games/Games.jsx
@@ -1,41 +1,38 @@
 import React from 'react';
 import GameCard from '../../components/GameCard/GameCard';
 import './Games.css';
+import memoryImage from '../../assets/memory.png';
+import tictactoeImage from '../../assets/tictactoe.png';
+import puzzle from '../../assets/puzzle.png';
+import wordImage from '../../assets/word.png';
 
 const games = [
   {
     id: 1,
     title: 'Hafıza Oyunu',
     description: 'Kartları eşleştirerek hafızanızı test edin.',
-    image: '/images/memory-game.jpg',
+    image: memoryImage,
     route: '/memoryGame'
   },
   {
     id: 2,
     title: 'Tic Tac Toe',
     description: 'Verilen ipuçlarına göre kelimeyi tahmin edin.',
-    image: '/images/word-game.jpg',
+    image: tictactoeImage,
     route: '/ticTacToe'
   },
   {
     id: 3,
-    title: 'Matematik Oyunu',
-    description: 'Matematik problemlerini çözerek puan toplayın.',
-    image: '/images/math-game.jpg',
-    route: '/mathGame'
-  },
-  {
-    id: 4,
     title: 'Sliding Image Puzzle',
     description: 'abc',
-    image: '/images/.jpg',
+    image: puzzle,
     route: '/slidingImagePuzzle'
   },
   {
-    id: 5,
+    id: 4,
     title: 'Word Find Game',
     description: 'abc',
-    image: '/images/.jpg',
+    image: wordImage,
     route: '/wordFindGame'
   },
 ];
@@ -43,7 +40,7 @@ const games = [
 const Games = () => {
   return (
     <div className="games">
-      <h1>Oyunlarımız</h1>
+      <h1>GAMES</h1>
       <div className="games-grid">
         {games.map((game) => (
           <GameCard key={game.id} {...game} />
--- games/src/components/Navbar/Navbar.jsx
+++ games/src/components/Navbar/Navbar.jsx
@@ -1,40 +1,32 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
-import search_icon_light from '../../assets/search-w.png'
-import SettingsIcon from '@mui/icons-material/Settings';
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom'; 
 
 function Navbar() {
   return (
-    <>
     <div className="navbar">
-        <img src={logo} alt="" className='logo'/>
-        <div className="logo-name"></div>
-        <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/games">Games</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-        </ul>
-        <div className="search-box">
-            <input type="text" placeholder='Search'/>
-            <img src={search_icon_light} alt="" />
-        </div>
-        <div className="settings-icon">
-            <SettingsIcon/>
-        </div>
+      <img src={logo} alt="Logo" className="logo" />
+      <div className="logo-name"></div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/games">Games</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="login-box">
+        <Link to="/login" className="login-link">Login</Link>
+      </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
